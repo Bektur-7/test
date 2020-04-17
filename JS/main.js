1e8a7167260da74ad25b9ea810fe3aa1da64d67e
Bektur Tologonov <tbektur7@gmail.com>
2020-04-17 20:19:22 +0600

    first commit

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+// function buttonClick (button) {
+//     alert("Вы нажали на кнопку. Кнопка имеет имя " + button.name + ", также value равно " + button.value + ".")
+
+// }
+// var counter = 0
+
+// function counter1(element) {
+    
+//     counter++
+
+//     element.innerHTML = "На этот текст было наведено "  + counter + " раз"
+// }
+
+
+// function hello() {
+//     console.log("Hello", this)
+// }
+
+
+
+// const person = {
+//     name: "Bektur",
+//     age: 26,
+//     sayHello: hello
+// }
+
+
